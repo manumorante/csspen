e81439a37a75a5a62b2558054df3c0facf1990e6
Manu Morante <manuel.morante@betvictor.com>
2017-03-02 15:07:22 +0100

    Prevent scroll

--- assets/application.js
+++ assets/application.js
@@ -47,6 +47,7 @@
 	'use strict';
 
 	__webpack_require__(1);
+	__webpack_require__(187);
 	__webpack_require__(2);
 	__webpack_require__(4);
 
@@ -32085,6 +32086,45 @@
 
 	module.exports = ReactDOMInvalidARIAHook;
 	/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(7)))
+
+/***/ },
+/* 187 */
+/***/ function(module, exports) {
+
+	'use strict';
+
+	module.exports = function (el) {
+	  el.addEventListener('touchstart', function () {
+	    var top = el.scrollTop,
+	        totalScroll = el.scrollHeight,
+	        currentScroll = top + el.offsetHeight;
+
+	    //If we're at the top or the bottom of the containers
+	    //scroll, push up or down one pixel.
+	    //
+	    //this prevents the scroll from "passing through" to
+	    //the body.
+	    if (top === 0) {
+	      el.scrollTop = 1;
+	    } else if (currentScroll === totalScroll) {
+	      el.scrollTop = top - 1;
+	    }
+	  });
+
+	  el.addEventListener('touchmove', function (evt) {
+	    //if the content is actually scrollable, i.e. the content is long enough
+	    //that scrolling can occur
+	    if (el.offsetHeight < el.scrollHeight) evt._isScroller = true;
+	  });
+	};
+
+	document.body.addEventListener('touchmove', function (evt) {
+	  //In this case, the default behavior is scrolling the body, which
+	  //would result in an overflow.  Since we don't want that, we preventDefault.
+	  if (!evt._isScroller) {
+	    evt.preventDefault();
+	  }
+	});
 
 /***/ }
 /******/ ]);
--- source/entry.js
+++ source/entry.js
@@ -1,3 +1,4 @@
 require('./stylesheets/application.scss');
+require('./javascripts/_bounce.js');
 require('./javascripts/steps.js');
 require('./javascripts/application.jsx');
