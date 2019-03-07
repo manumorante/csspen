5d995e1e26f18a16e6fbd2891c96cb2037b59f0b
Manu Morante <manu@estadologico.com>
2019-03-07 21:30:39 +0100

    Remove react-tag-style module

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './css/styles.scss'
 
 ReactDOM.render(
     <div id="index">
-        <Results step="5" />
+        <Results steps={8} />
     </div>,
     document.getElementById("root")
 );
--- src/js/Results.js
+++ src/js/Results.js
@@ -1,24 +1,56 @@
 import React from "react";
-import { Style } from "react-style-tag";
+// import { Style } from "react-style-tag";
 
 export class Results extends React.Component {
   constructor(props) {
     super(props);
-    this.step = this.props.step;
-    this.itemStyles = this.getStyles();
+    this.state = {
+      step: 0,
+      steps: this.props.steps
+    }
+    this.pen = this.getStyles();
+    this.nextStep = this.nextStep.bind(this);
+    this.prevStep = this.prevStep.bind(this);
   }
 
   // Read styles form html into site
   getStyles() {
-    let css = document.getElementById('init-styles').textContent;
-    return css.split('/*-*/');
+    let $initStylesNone = document.getElementById('init-styles');
+    let cssText ='';
+
+    if($initStylesNone) {
+      cssText = $initStylesNone.textContent.split('/*-*/');
+      document.getElementById("body").removeChild($initStylesNone)
+    } else {
+      console.log('Initial styles node not found.')
+    }
+    return cssText;
+  }
+
+  nextStep(){
+    if(this.state.step < this.state.steps) {
+      this.setState({step: ++this.state.step})
+    }
+  }
+
+  prevStep() {
+    if(this.state.step > 0) {
+      this.setState({step: --this.state.step})
+    }
   }
 
   render() {
     return (
-      <div className="result">
-        <div className="result-body"></div>
-        <Style>{`${this.itemStyles[this.step]}`}</Style>
+      <div id="Results">
+        <button onClick={this.prevStep}>Prev</button>
+        <button onClick={this.nextStep}>Next</button>
+
+        <div className="playground">
+          <div className="heart">
+            <div className="heart-body"></div>
+          </div>
+        </div>        
+        <style>{this.pen[this.state.step]}</style>
       </div>
     );
   }
