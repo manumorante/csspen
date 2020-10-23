14969d172e6ed5bacf1c03853ba2f2baf958dee6
Manu <manu@estadologico.com>
2020-10-23 14:15:01 +0200

    Clean main branch

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { App } from "./js/App";
-import './css/styles.scss'
-
-ReactDOM.render(
-    <App totalSteps={8} />,
-    document.getElementById("root")
-);
