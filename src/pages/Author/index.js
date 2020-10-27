b9b38af837ccf5b246ad7fe28594b6b2bf1750f1
Manu <manu@estadologico.com>
2020-10-27 18:34:54 +0100

    Add simple bio

--- src/pages/Author/index.js
+++ src/pages/Author/index.js
@@ -8,6 +8,8 @@ export default function Author () {
     <div className='Page Author'>
       <h2 className='Page__title'>Author</h2>
       <Avatar url={avatarManu} />
+      <p>I'm passionate about creating applications. I have working between design and front end for more than twelve years. Doing very specific things for the Github community and managing a multidisciplinary team.</p>
+      <p>In recent years I have worked creating design systems, both in HTML development, SASS, etc. and in visual design of components using Sketch. </p>
     </div>
   )
 }
