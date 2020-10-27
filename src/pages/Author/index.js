009bfd20fe69884a34a799c0ac27ff99383a0bf2
Manu <manu@estadologico.com>
2020-10-27 18:33:03 +0100

    Add Avatar to Author page

--- src/pages/Author/index.js
+++ src/pages/Author/index.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import Avatar from '../../components/Avatar'
+
+const avatarManu = 'https://secure.gravatar.com/avatar/3d6f5d17df19913a7a7970923563710e'
 
 export default function Author () {
   return (
     <div className='Page Author'>
       <h2 className='Page__title'>Author</h2>
+      <Avatar url={avatarManu} />
     </div>
   )
 }
