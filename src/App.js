85a526164c4a73ce3a0fffb8a9fbb3e6bea06150
Manu <manu@estadologico.com>
2020-10-27 20:20:37 +0100

    Delete unnecessary context

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Route, Router } from 'wouter'
 import useHashLocation from './hooks/useHashLocation'
-import { PensContextProvider } from './context/PensContext'
 import Home from './pages/Home'
 import Playground from './pages/Playground'
 import Author from './pages/Author'
@@ -10,15 +9,13 @@ import Header from './components/Header'
 export default function App() {
   return (
     <div id='App' className='app'>
-      <PensContextProvider>
-        <Router hook={useHashLocation}>
-          <Header/>
+      <Router hook={useHashLocation}>
+        <Header/>
 
-          <Route path='/' component={Home} />
-          <Route path='/pen/:id' component={Playground} />
-          <Route path='/author' component={Author} />
-        </Router>
-      </PensContextProvider>
+        <Route path='/' component={Home} />
+        <Route path='/pen/:id' component={Playground} />
+        <Route path='/author' component={Author} />
+      </Router>
     </div>
   )
 }
--- src/hooks/usePens.js
+++ src/hooks/usePens.js
@@ -1,10 +1,9 @@
-import {useContext, useEffect, useState} from 'react'
+import { useEffect, useState } from 'react'
 import getPens from '../services/getPens'
-import pensContext from '../context/PensContext'
 
 export function usePens () {
   const [loading, setLoading] = useState(false)
-  const {pens, setPens} = useContext(pensContext)
+  const [pens, setPens] = useState([])
 
   useEffect(function () {
     setLoading(true)
