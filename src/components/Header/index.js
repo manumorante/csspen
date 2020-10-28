bb925abf39b9448e8e061023e26707a9507e973e
Manu <manu@estadologico.com>
2020-10-28 22:11:47 +0100

    Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import './styles.css'
+import Logo from '../Logo'
 import Menu from '../Menu'
-import { Link } from 'wouter'
 
 export default function Header () {
   return (
     <div className='Header'>
-      <h1 className='Header__title'><Link to='/'>CSS.Learn</Link></h1>      
+      <Logo/>
       <Menu/>
     </div>
   )
