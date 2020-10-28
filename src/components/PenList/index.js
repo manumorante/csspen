ccff87cf60616724ba89cf7e6ea2ef4f01c61946
Manu Morante <manu@estadologico.com>
2020-10-28 19:02:39 +0000

    Change name ListOfPen to PenList

---
+++ src/components/PenList/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'wouter';
+import './styles.css'
+
+export default function PenList ({ pens }) {
+  return <div className='PenList'>
+    {pens.map(({ id, name, description }) => {
+      return <Link className='PenList__item' key={id} to={`/pen/${id}`}>
+        <div className='PenList__title'>{name}</div>
+        <div className='PenList__description'>{description}</div>
+      </Link>
+    })}
+  </div>
+}
