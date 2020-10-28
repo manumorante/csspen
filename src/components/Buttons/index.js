e7a701a976ee2854f287ea75f0783dd1ad5b5364
Manu <manu@estadologico.com>
2020-10-28 22:11:23 +0100

    Buttons

---
+++ src/components/Buttons/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './styles.css'
+
+export default function Buttons ({ children }) {
+  return (
+    <div className='Buttons'>
+      {children}
+    </div>
+  )
+}
