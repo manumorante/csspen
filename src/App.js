3711371c4020ef8a5abd22b510d52976e5562951
Manu Morante <manu@estadologico.com>
2022-02-10 21:06:31 +0100

    Do not use Wouter

--- src/App.js
+++ src/App.js
@@ -1,15 +1,66 @@
-import React from 'react'
-import { Route, Router } from 'wouter'
-import useHashLocation from './hooks/useHashLocation'
-import Pen from './components/Pen'
+import React, { useState, useEffect } from 'react'
+import { usePens } from './hooks/usePens'
+import PenCard from './components/PenCard'
+import Editor from './components/Editor'
+import createPen from './services/createPen'
 
-export default function App() {
+const DEFAULT_PEN_ID = 'heart'
+
+/**
+ * PEN (PAGE)
+ * - Este componente se llama por url con un id del Pen deseado
+ * - Si no recibe un id, se define uno por defecto
+ * - Carga todos los pens usando el curstom hook usePens
+ * - Busca el pen con el id recibido en la lista de pens
+ */
+export default function App () {
+  const currentHash = () => window.location.hash.replace('#', '') || DEFAULT_PEN_ID
+  const [penID, setPenID] = useState(currentHash())
+  const {loadingPens, pens} = usePens()
+  const [pen, setPen] = useState(false)
+
+  useEffect(() => {
+    // Subscribe on hash changes
+    const handlerHashChange = () => setPenID(currentHash())
+    window.addEventListener('hashchange', handlerHashChange)
+    
+    // Close pen list menu when pen is selected
+    handleClosePenList()
+
+    return () => window.removeEventListener('hashchange', handlerHashChange)
+  }, [penID])
+
+  useEffect(() => {
+    if(!loadingPens && pens){
+      const newPen = createPen(penID, pens)
+
+      if(!newPen) {
+        console.log('useEffect [loadingPens, pens, id] - Pen not found')
+        return false
+      }
+
+      setPen(newPen)
+    }
+  }, [loadingPens, pens, penID])
+
+  const handleClosePenList = () => {
+    document.querySelector('body').classList.remove('show-pen-list')
+  }
+
   return (
-    <div id='App' className='App'>
-      <Router hook={useHashLocation}>
-        <Route path='/' component={Pen} />
-        <Route path='/:id' component={Pen} />
-      </Router>
+    <div className='Pen'>
+      { loadingPens
+      ? <div className='Spinner' />
+      : <div className='PenList'>
+          <button className='Button PenList__close' onClick={handleClosePenList}>Close</button>
+
+          {pens.map((item) => {
+            return <PenCard key={item.id} pen={item} active={penID} />
+          })}
+        </div>
+      }
+
+      { pen ? <Editor pen={pen} /> : <b>Pen not found</b> }
     </div>
   )
 }
--- src/components/PenCard.js
+++ src/components/PenCard.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import { Link } from 'wouter'
+// import { Link } from 'wouter'
 
-export default function PenCard ({pen, active}) {
+export default function PenCard ({ pen, active }) {
   const { id, name, info } = pen
 
-  return <Link className={`PenCard ${(id === active) ? 'active' : ''}`} key={id} to={`/${id}`}>
+  return <a href={`#${id}`} className={`PenCard ${(id === active) ? 'active' : ''}`} key={id}>
   <div className='PenCard__title'>{name}</div>
   <div className='PenCard__description'>{info}</div>
-</Link>
+</a>
 }
