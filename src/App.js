5027e51a75296f129113184fbbbcc0ab1464a0fb
Manu Morante <manu@estadologico.com>
2022-02-20 19:32:08 +0100

    Move responsability to app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-/**
- * CSS Learn
- * - Este componente se llama por url con un id del Pen deseado
- * - Si no recibe un id, se define uno por defecto
- * - Carga todos los pens usando el curstom hook usePens
- * - Busca el pen con el id recibido en la lista de pens
- */
-
 import React, { useState, useEffect } from 'react'
+import { parseCSS } from './js/parseCSS'
 import { usePens } from './js/usePens'
-import PenCard from './components/PenCard'
-import Editor from './components/Editor'
 import createPen from './js/createPen'
+import PenCard from './components/PenCard'
+import Tag from './components/Tag'
+import Code from './components/Code'
+import Button from './components/Button'
+
+  // TODO:
+  // - check here (in Editor component) if pen is valid?
+  // - Usar reducer para unificar estados
+  // - Placeholder loading
 
 const DEFAULT_PEN_ID = 'heart'
 
@@ -19,6 +19,12 @@ export default function App () {
   const [penID, setPenID] = useState(currentHash())
   const {loadingPens, pens} = usePens()
   const [pen, setPen] = useState(false)
+  const [step, setStep] = useState(0)
+  const [totalSteps, setTotalSteps] = useState(0)
+  const [stepInfo, setStepInfo] = useState()
+  const [rawCode, setRawCode] = useState()
+  const [parsedCode, setParsedCode] = useState()
+  const [autoplay, setAutoplay] = useState()
 
   useEffect(() => {
     // Subscribe on hash changes
@@ -41,8 +47,77 @@ export default function App () {
       }
 
       setPen(newPen)
+      setStep(0)
+      setTotalSteps(newPen.steps.length)
+      setStepInfo(newPen.steps[0].info)
+      setRawCode(newPen.steps[0].code)
+      setParsedCode(parseCSS(newPen.steps[0].code))
+      setAutoplay(true)
     }
   }, [loadingPens, pens, penID])
+
+  // When step changes
+  useEffect(() => {
+    if(!pen) return false
+
+    const newStep = pen.steps[step]
+    if(!newStep) return
+
+    const newInfo = newStep?.info || `Step ${step + 1}`
+
+    setRawCode(newStep.code)
+    setParsedCode(parseCSS(newStep.code))
+    setStepInfo(newInfo)
+  }, [step])
+
+  // Play
+  useEffect(() => {
+    if(autoplay) {
+      const timeout = setTimeout(() => {
+        if (step >= pen.steps.length - 1) {
+          setAutoplay(false)
+        } else {
+          setStep(step => step + 1)
+        }
+      }, 1000)
+
+      return () => {
+        // useEffect callback return function
+        clearTimeout(timeout)
+      }
+    }
+  }, [autoplay, step, pen.steps])
+
+  function handlePlayStop() {
+    const newAutoplay = !autoplay
+    setAutoplay(newAutoplay)
+  }
+
+  // Functions to check is can move to next or previous step
+  const notNext = () => step + 1 >= totalSteps
+  const notPrev = () => step <= 0
+
+  function handleNext() {
+    if (notNext()) return
+
+    setAutoplay(false)
+    setStep(step => step + 1)
+  }
+
+  function handlePrev() {
+    if (notPrev()) return
+
+    setAutoplay(false)
+    setStep(step => step - 1)
+  }
+
+  // Mobile menu with the list of Pens
+  function handleMore() {
+    // TODO: use State, Context, ...
+    document.querySelector('body').classList.add('show-pen-list')
+  }
+
+  const handleUpdateRawCode = setRawCode
 
   const handleClosePenList = () => {
     document.querySelector('body').classList.remove('show-pen-list')
@@ -52,7 +127,6 @@ export default function App () {
     return <div className='Spinner' />
   }
 
-  // TODO: crear loadings dentro de cada componente: se pinta tipo placeholder y cuando tiene el contenido lo muestra
   return (
     <div className='App'>
       <div className='PenList'>
@@ -61,10 +135,33 @@ export default function App () {
         {pens.map((item) => <PenCard key={item.id} pen={item} active={penID} />)}
       </div>
 
-      <div className='Editor'>
-      </div>
+      <div className='Editor' style={{background: pen.bg}}>
+        <div className='Editor__code'>
+          <div className='Editor__step-info'>{stepInfo}</div>
 
-      { <Editor pen={pen} /> }
+          <div className='Code'>
+            <Code parsedCode={parsedCode} handleUpdateRawCode={handleUpdateRawCode} />
+
+            <Tag html={`<style type="text/css">${rawCode}</style>`} />
+          </div>
+
+          <div className='Buttons Editor__buttons'>
+            { totalSteps ?
+              <>
+                <Button label={autoplay ? 'Stop' : 'Play'} action={handlePlayStop} />
+                <Button label={`${step + 1}/${totalSteps}`} disabled={true} />
+                <Button label='<' action={handlePrev} disabled={notPrev()} />
+                <Button label='>' action={handleNext} disabled={notNext()} />
+              </>
+              :
+              <Button label='Fixed paint' disabled={true} />
+            }
+            <Button className='button--more' label='More!' action={handleMore} />
+          </div>
+        </div>
+
+        <Tag html={pen.html} className='Editor__html' />
+      </div>
     </div>
   )
 }
--- src/components/Code.js
+++ src/components/Code.js
@@ -1,22 +1,20 @@
-import hljs from 'highlight.js'
 import React, { useEffect, useRef } from 'react'
-import Tag from './Tag'
+import hljs from 'highlight.js'
 import 'highlight.js/styles/atom-one-dark.css'
 
 /**
  *
- * @param children CSS raw code from the pen.json
  * @param parsedCode Parsed CSS code
  * @param handleUpdateRawCode Method to update the state in the parent component
  */
-export default function Code ({ children, parsedCode, handleUpdateRawCode }) {
+export default function Code ({ parsedCode, handleUpdateRawCode }) {
   const codeTag = useRef()
 
   useEffect(() => {
     const timeout = setTimeout(() => hljs.highlightBlock(codeTag.current), 0)
 
     return () => clearTimeout(timeout)
-  }, [children])
+  }, [parsedCode])
 
   function update() {
     handleUpdateRawCode(codeTag.current.textContent)
@@ -27,21 +25,17 @@ export default function Code ({ children, parsedCode, handleUpdateRawCode }) {
   }
 
   return (
-    <div className="Code">
-      <pre className="Code__pre">
-        <code
-          ref={codeTag}
-          className="Code__tag css"
-          onBlur={handleBlur}
-          contentEditable="true"
-          suppressContentEditableWarning="true"
-          autoCorrect="off"
-          autoComplete="off"
-          autoCapitalize="off"
-          spellCheck="false">{parsedCode}</code>
-      </pre>
-
-      <Tag html={`<style type="text/css">${children}</style>`} />
-    </div>
+    <pre className="Code__pre">
+      <code
+        ref={codeTag}
+        className="Code__tag css"
+        onBlur={handleBlur}
+        contentEditable="true"
+        suppressContentEditableWarning="true"
+        autoCorrect="off"
+        autoComplete="off"
+        autoCapitalize="off"
+        spellCheck="false">{parsedCode}</code>
+    </pre>
   )
 }
