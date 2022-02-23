088e9d29bf5defe3895dc844373f7532edd79b30
Manu Morante <manu@estadologico.com>
2022-02-23 13:11:38 +0100

    build

---
+++ build/precache-manifest.cf2d75609b04400f60188ed7512a251b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b8bcaae7776ed624e30917c58391b77",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f42e2f276526e74637f8",
+    "url": "/static/css/2.72d72b5e.chunk.css"
+  },
+  {
+    "revision": "101e578764e40010b25c",
+    "url": "/static/css/main.bc924b25.chunk.css"
+  },
+  {
+    "revision": "f42e2f276526e74637f8",
+    "url": "/static/js/2.5d1fecb5.chunk.js"
+  },
+  {
+    "revision": "5a81c71b9c00462c5a89dfae5226e5eb",
+    "url": "/static/js/2.5d1fecb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "101e578764e40010b25c",
+    "url": "/static/js/main.fc9e66c9.chunk.js"
+  },
+  {
+    "revision": "b83b940580a16fd26f14",
+    "url": "/static/js/runtime-main.2ebf7936.js"
+  }
+]);
