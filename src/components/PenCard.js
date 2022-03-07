cc2a13e6f59a2a79043296fc7a38f43c388dcd73
Manu Morante <manu@estadologico.com>
2022-03-07 11:38:58 +0100

    Fix css name

--- src/components/PenCard.js
+++ src/components/PenCard.js
@@ -3,8 +3,8 @@ import React from 'react'
 export default function PenCard({ pen, isActive = false }) {
   return (
     <a href={`#${pen.id}`} className={`PenCard ${isActive ? 'active' : ''}`}>
-      <div className='PenCard__title'>{pen.name}</div>
-      <div className='PenCard__description'>{pen.info}</div>
+      <div className='PenCard__name'>{pen.name}</div>
+      <div className='PenCard__info'>{pen.info}</div>
     </a>
   )
 }
