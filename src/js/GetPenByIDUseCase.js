6148ad31750577701730a945f5e397beaf37ccdb
Manu Morante <manu@estadologico.com>
2022-03-11 19:23:36 +0100

    Promise.all()

--- src/js/GetPenByIDUseCase.js
+++ src/js/GetPenByIDUseCase.js
@@ -4,11 +4,13 @@ import { penMapper } from '../js/penMapper'
 export class GetPenByIDUseCase {
   async execute({ penID }) {
     const repository = new PenRepository()
-    // TODO use Promise.all()
-    const pen = await repository.getPen(penID)
-    const steps = await repository.getSteps(penID)
-    const penMapped = penMapper(pen, steps)
 
+    const [pen, steps] = await Promise.all([
+      repository.getPen(penID),
+      repository.getSteps(penID),
+    ])
+
+    const penMapped = penMapper(pen, steps)
     return penMapped
   }
 }
