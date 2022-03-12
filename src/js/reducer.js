8242e16783bc7f465fcf42c5721574539c018330
Manu Morante <manu@estadologico.com>
2022-03-12 14:51:08 +0100

    Move reducer actions to functions

--- src/js/reducer.js
+++ src/js/reducer.js
@@ -1,123 +1,169 @@
+import { A } from './reducerActions'
 import parseCSS from './parseCSS'
 import { UpdateStepUseCase } from './UpdateStepUseCase'
 
-// TODO: usar un objeto actions `const actions = { SET = 'SET', ... }`
-// Action types:
-// SET_ - Guarda o actualiza el estado en memoria
-// UPDATE_ - Actualiza en la base de datos
+// TODOs:
+// - Cada case deberia ir en una funcion...
+
+const getStep = (steps, stepNumber) => {
+  const step = steps[stepNumber]
+  return step || false
+}
+
+const getStepProp = (steps, stepNumber, prop) => {
+  const step = getStep(steps, stepNumber)
+  return step && step[prop]
+}
+
+const getStepInfo = (steps, stepNumber) =>
+  getStepProp(steps, stepNumber, 'info')
+
+const getStepCode = (steps, stepNumber) =>
+  getStepProp(steps, stepNumber, 'code')
+
+const canEdit = (state) => state.email === 'manu@estadologico.com'
+
+const reducers = {
+  // UI
+  [A.LOADING]: (state, action) => {
+    return { ...state, loading: true }
+  },
+
+  [A.WRITING]: (state, action) => {
+    return { ...state, writing: true, autoplay: false }
+  },
+
+  [A.SHOW_MENU]: (state, action) => {
+    return { ...state, menu: 'action-menu', autoplay: false }
+  },
+
+  [A.HIDE_MENU]: (state, action) => {
+    return { ...state, menu: '' }
+  },
+
+  // NAVIGATION
+  [A.NEXT]: (state, action) => {
+    return {
+      ...state,
+      step: state.step < state.totalSteps - 1 ? state.step + 1 : state.step,
+    }
+  },
+
+  [A.PREV]: (state, action) => {
+    return {
+      ...state,
+      step: state.step > 0 ? state.step - 1 : state.step,
+    }
+  },
+
+  [A.REWIND]: (state, action) => {
+    return {
+      ...state,
+      step: state.totalSteps - 1,
+      autoplay: false,
+      rewind: true,
+    }
+  },
+
+  [A.PLAY]: (state, action) => {
+    return { ...state, step: 0, autoplay: true, rewind: false }
+  },
+
+  [A.STOP]: (state, action) => {
+    return { ...state, autoplay: false, rewind: false }
+  },
+
+  [A.PLAY_STOP]: (state, action) => {
+    return { ...state, autoplay: !state.autoplay, rewind: false }
+  },
+
+  // SET CURRENT
+  [A.SET_PEN]: (state, action) => {
+    return {
+      ...state,
+      ...action.pen,
+      email: action.email,
+      loading: false,
+      loaded: true,
+      rewind: false,
+      writing: false,
+      edited: false,
+      step: 0,
+      totalSteps: action.pen.totalSteps,
+      stepInfo: getStepInfo(action.pen.steps, 0),
+      rawCode: getStepCode(action.pen.steps, 0),
+      parsedCode: parseCSS(getStepCode(action.pen.steps, 0)),
+      menu: '',
+    }
+  },
+
+  [A.SET_STEP]: (state, action) => {
+    return {
+      ...state,
+      stepInfo: state.steps[state.step].info,
+      rawCode: state.steps[state.step].code,
+      parsedCode: parseCSS(state.steps[state.step].code),
+      menu: '',
+    }
+  },
+
+  [A.SET_STEP_CODE]: (state, action) => {
+    // Optimizar esto con `...`
+    const steps = state.steps
+    steps[state.step].code = action.code
+
+    return {
+      ...state,
+      writing: false,
+      edited: true,
+      steps: steps,
+      rawCode: action.code,
+      parsedCode: parseCSS(action.code),
+    }
+  },
+
+  [A.SET_STEP_INFO]: (state, action) => {
+    // Optimizar esto con `...`
+    const steps = state.steps
+    steps[state.step].info = action.stepInfo
+
+    return {
+      ...state,
+      writing: false,
+      edited: true,
+      steps: steps,
+      stepInfo: action.stepInfo,
+    }
+  },
+
+  // CREATE AND EDIT NEW ONES
+  [A.NEW_STEP]: (state, action) => {
+    return {
+      ...state,
+      step: state.totalSteps + 1,
+      totalSteps: state.totalSteps + 1,
+      stepInfo: state.steps[state.step].info,
+      rawCode: state.steps[state.step].code,
+      parsedCode: parseCSS(state.steps[state.step].code),
+    }
+  },
+
+  [A.UPDATE_STEP]: (state, action) => {
+    if (!canEdit(state)) return state
+
+    const UpdateStep = new UpdateStepUseCase()
+    UpdateStep.execute({
+      penID: state.id,
+      step: state.step,
+      code: state.rawCode,
+      info: state.stepInfo,
+    }).then(() => console.log('Saved'))
+
+    return { ...state, edited: false }
+  },
+}
 
 export function reducer(state, action) {
-  switch (action.type) {
-    case 'SET_PEN':
-      return {
-        ...state,
-        ...action.pen,
-        email: action.email,
-        loading: false,
-        loaded: true,
-        rewind: false,
-        writing: false,
-        edited: false,
-        step: 0,
-        totalSteps: action.pen.totalSteps,
-        stepInfo: action.pen.steps[0].info,
-        rawCode: action.pen.steps[0].code,
-        parsedCode: parseCSS(action.pen.steps[0].code),
-        menu: '',
-      }
-
-    case 'SET_STEP':
-      return {
-        ...state,
-        stepInfo: state.steps[state.step].info,
-        rawCode: state.steps[state.step].code,
-        parsedCode: parseCSS(state.steps[state.step].code),
-        menu: '',
-      }
-
-    case 'UPDATE_STEP':
-      // TODO: :face_palm:
-      if (state.email === 'manu@estadologico.com') {
-        const UpdateStep = new UpdateStepUseCase()
-        UpdateStep.execute({
-          penID: state.id,
-          step: state.step,
-          code: state.rawCode,
-          info: state.stepInfo,
-        }).then(() => console.log('Saved'))
-      }
-
-      return { ...state, edited: false }
-
-    case 'SET_STEP_CODE':
-      // Optimizar esto con `...`
-      const UPDATE_STEP_CODE_newSteps = state.steps
-      UPDATE_STEP_CODE_newSteps[state.step].code = action.code
-
-      return {
-        ...state,
-        writing: false,
-        edited: true,
-        steps: UPDATE_STEP_CODE_newSteps,
-        rawCode: action.code,
-        parsedCode: parseCSS(action.code),
-      }
-
-    case 'SET_STEP_INFO':
-      // Optimizar esto con `...`
-      const UPDATE_STEP_INFO_newSteps = state.steps
-      UPDATE_STEP_INFO_newSteps[state.step].info = action.stepInfo
-
-      return {
-        ...state,
-        writing: false,
-        edited: true,
-        steps: UPDATE_STEP_INFO_newSteps,
-        stepInfo: action.stepInfo,
-      }
-
-    case 'LOADING':
-      return { ...state, loading: true }
-
-    case 'WRITING':
-      return { ...state, writing: true, autoplay: false }
-
-    case 'NEXT':
-      return {
-        ...state,
-        step: state.step < state.totalSteps - 1 ? state.step + 1 : state.step,
-      }
-
-    case 'PREV':
-      return {
-        ...state,
-        step: state.step > 0 ? state.step - 1 : state.step,
-      }
-
-    case 'REWIND':
-      return {
-        ...state,
-        step: state.totalSteps - 1,
-        autoplay: false,
-        rewind: true,
-      }
-
-    case 'PLAY_STOP':
-      return { ...state, autoplay: !state.autoplay, rewind: false }
-
-    case 'PLAY':
-      return { ...state, step: 0, autoplay: true, rewind: false }
-
-    case 'STOP':
-      return { ...state, autoplay: false, rewind: false }
-
-    case 'SHOW_MENU':
-      return { ...state, menu: 'ac-menu', autoplay: false }
-
-    case 'HIDE_MENU':
-      return { ...state, menu: '' }
-
-    default:
-      throw new Error()
-  }
+  const actionReducer = reducers[action.type]
+  return actionReducer ? actionReducer(state, action) : state
 }
---
+++ src/js/reducerActions.js
@@ -0,0 +1,25 @@
+export const A = {
+  // UI
+  LOADING: 'LOADING',
+  WRITING: 'WRITING',
+  SHOW_MENU: 'SHOW_MENU',
+  HIDE_MENU: 'HIDE_MENU',
+
+  // Navigate
+  NEXT: 'NEXT',
+  PREV: 'PREV',
+  REWIND: 'REWIND',
+  PLAY: 'PLAY',
+  STOP: 'STOP',
+  PLAY_STOP: 'PLAY_STOP',
+
+  // Set current
+  SET_PEN: 'SET_PEN',
+  SET_STEP: 'SET_STEP',
+  SET_STEP_CODE: 'SET_STEP_CODE',
+  SET_STEP_INFO: 'SET_STEP_INFO',
+
+  // Create or edit new ones
+  NEW_STEP: 'NEW_STEP',
+  UPDATE_STEP: 'UPDATE_STEP',
+}
