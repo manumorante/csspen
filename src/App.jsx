1b5f6e14f887e704a904043bfdc6e0a913e0b56f
Manu Morante <manu@estadologico.com>
2022-03-14 12:39:35 +0100

    Redirect to pens

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
 import Profile from './components/Profile'
-import Home from './pages/Home'
 import Pen from './pages/Pen'
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path='/' component={Home} exact />
+        <Redirect from='/' to='/pen/heart' exact />
         <Route path='/pen/:slug' component={Pen} />
         <Route path='/profile' component={Profile} />
       </Switch>
