bc5a5731a44a42f0cb63c12dd2baf0fe942ce6fd
Manu Morante <manu@estadologico.com>
2022-03-14 18:07:16 +0100

    Vite uses dotdev

--- src/js/UserProvider.jsx
+++ src/js/UserProvider.jsx
@@ -2,8 +2,8 @@ import React, { useState, useEffect, useContext } from 'react'
 import { createClient } from '@supabase/supabase-js'
 
 const client = createClient(
-  'https://qxgdolgewtvoyrwdbwdo.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF4Z2RvbGdld3R2b3lyd2Rid2RvIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDU3OTIyMTgsImV4cCI6MTk2MTM2ODIxOH0.HFgR00_0kY41zY6zwu7HZaz9JBEgqcj6dXSKKDn7Q0Q'
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_KEY
 )
 
 export const userContext = React.createContext()
@@ -23,7 +23,5 @@ export function UserProvider({ children }) {
     })
   }, [session])
 
-  return (
-    <userContext.Provider value={session}>{children}</userContext.Provider>
-  )
+  return <userContext.Provider value={session}>{children}</userContext.Provider>
 }
