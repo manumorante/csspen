3ece2732268f1349fb2d44413b20807e74280f37
Manu Morante <manu@estadologico.com>
2022-03-17 10:01:19 +0100

    Fix styles

--- src/styles.js
+++ src/styles.js
@@ -1,7 +1,7 @@
 export const layout = {
   main: 'w-screen h-screen flex flex-col overflow-hidden',
   header: 'h-10 flex-none',
-  pen: 'flex-grow flex flex-col sm:flex-row',
+  pen: 'flex-grow flex flex-col sm:flex-row sm:w-full sm:h-full',
   menu: {
     base: 'sm:block sm:relative sm:w-52 bg-neutral-900',
     closed: 'hidden',
@@ -12,8 +12,8 @@ export const layout = {
   editor:
     'flex flex-col overflow-hidden h-1/2 sm:w-96 sm:h-full sm:flex sm:flex-col',
   stage: {
-    base: 'flex-grow',
-    html: 'grid place-items-center h-screen',
+    base: 'flex-grow ',
+    html: 'grid place-items-center h-full',
   },
   footer: 'h-10 flex-none',
 }
