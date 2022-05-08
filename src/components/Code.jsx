c09a1ff2755f3b7acc9401e5ea510ed095ed7221
Manu Morante <manu@estadologico.com>
2022-05-08 22:08:32 +0200

    Improve editor

--- src/components/Code.jsx
+++ src/components/Code.jsx
@@ -6,26 +6,42 @@ import 'highlight.js/styles/atom-one-dark.css'
 export default function Code({ state, dispatch }) {
   const codeTag = useRef()
 
-  if (!state.pen || Object.keys(state.pen).length === 0)
-    return <div>Loading ...</div>
+  useEffect(() => {
+    codeTag.current.textContent = parseCSS(code)
+    hljs.highlightElement(codeTag.current)
+  }, [state.step])
 
   let code = ''
-
   try {
     code = parseCSS(state.pen.pen_steps[state.step].code)
   } catch (error) {}
 
-  useEffect(() => {
-    hljs.highlightElement(codeTag.current)
-  }, [code])
+  const hasChanged = () => {
+    return code !== codeTag.current.textContent
+  }
+
+  // SET_STEP_CODE
+  let preventTimeout = null
+  const preventTime = 800
+  const handleInput = () => {
+    clearInterval(preventTimeout)
+    preventTimeout = setTimeout(() => {
+      if (hasChanged()) {
+        dispatch({ type: 'SET_STEP_CODE', code: codeTag.current.textContent })
+      }
+    }, preventTime)
+  }
 
   const handleFocus = () => {
     dispatch({ type: 'WRITING' })
   }
 
   const handleBlur = () => {
-    dispatch({ type: 'SET_STEP_CODE', code: codeTag.current.textContent })
+    codeTag.current.textContent = parseCSS(codeTag.current.textContent)
+    hljs.highlightElement(codeTag.current)
   }
+
+  if (!state.pen || Object.keys(state.pen).length === 0) return
 
   return (
     <pre className={`Code flex-grow overflow-scroll`}>
@@ -34,14 +50,13 @@ export default function Code({ state, dispatch }) {
         className='css outline-none'
         onFocus={handleFocus}
         onBlur={handleBlur}
+        onInput={handleInput}
         contentEditable='true'
         suppressContentEditableWarning='true'
         autoCorrect='off'
         autoComplete='off'
         autoCapitalize='off'
-        spellCheck='false'>
-        {code}
-      </code>
+        spellCheck='false'></code>
     </pre>
   )
 }
