cbeed1ceda5347412c00f4a314255d2aef1d7482
Manu Morante <manu@estadologico.com>
2022-05-08 20:47:31 +0200

    Clean

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Pen from './pages/Pen'
-import Profile from './pages/Profile'
 import Stories from './pages/Stories'
 
 function App() {
@@ -11,7 +10,6 @@ function App() {
         <Routes>
           <Route path='/' element={<Navigate replace to='/pen/heart' />} />
           <Route path='/pen/:slug' element={<Pen />} />
-          <Route path='/profile' element={<Profile />} />
           <Route path='/stories' element={<Stories />} />
           <Route path='*' element={<Navigate replace to='/' />} />
         </Routes>
