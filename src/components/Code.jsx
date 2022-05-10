aaa779e02a32bff95a087af38bcc851719ed7e4f
Manu Morante <manu@estadologico.com>
2022-05-10 19:16:05 +0200

    Remove css parser

--- src/components/Code.jsx
+++ src/components/Code.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Editor from 'react-simple-code-editor'
 import { highlight, languages } from 'prismjs/components/prism-core'
 import Loading from './Loading'
-import parseCSS from '../js/parseCSS'
 import 'prismjs/components/prism-clike'
 import 'prismjs/components/prism-css'
 import '../prism-atom-dark.css'
@@ -10,19 +9,16 @@ import '../prism-atom-dark.css'
 export default function Code({ state, dispatch }) {
   const { pen } = state
   const code = pen.pen_steps[state.step]?.code
-  const parserCode = parseCSS(code)
-  
+
   const handleValueChange = (value) => {
     dispatch({ type: 'SET_STEP_CODE', code: value })
   }
-
-  // loading up to this
 
   return (
     <Loading until={state.loaded}>
       <Editor
         className='editor-css'
-        value={parserCode}
+        value={code}
         onValueChange={(value) => handleValueChange(value)}
         highlight={(value) => highlight(value, languages.css)}
         padding={14}
