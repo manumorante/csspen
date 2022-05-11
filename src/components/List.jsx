d713706afe2f4bf157b2ce0515e1cd0d831167bb
Manu Morante <manu@estadologico.com>
2022-05-11 19:46:19 +0200

    Use Card placeholder

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useApiContext } from '../context/ApiContext'
 import Card from './Card'
+import CardPH from './CardPH'
 import Loading from './Loading'
 
 export default function List({ active }) {
@@ -8,10 +9,12 @@ export default function List({ active }) {
 
   return (
     <Loading until={state.loaded}>
-      <div className='absolute z-20 top-0 left-0 w-full h-full overflow-y-auto flex flex-col'>
+      <div className='absolute z-20 top-0 left-0 w-full h-full overflow-y-auto flex flex-col mb-96'>
         {state.pens.map((pen) => (
           <Card key={pen.id} pen={pen} isActive={pen.id === active} />
         ))}
+        <CardPH />
+        <CardPH />
       </div>
     </Loading>
   )
