acf08d9670217f49ddc65952bbdda4fe3b71f426
Manu Morante <manu@estadologico.com>
2022-08-19 18:06:56 +0200

    meta

--- pages/_app.js
+++ pages/_app.js
@@ -1,25 +1,10 @@
-import Script from 'next/script'
+import Meta from '../components/app/Meta'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      {/* Global site tag (gtag.js) - Google Analytics */}
-      <Script
-        async
-        src='https://www.googletagmanager.com/gtag/js?id=G-FZMR8SL35B'
-      />
-      <Script id='gtag-base' strategy='afterInteractive'>
-        {`
-        window.dataLayer = window.dataLayer || []
-        function gtag() {
-          dataLayer.push(arguments)
-        }
-        gtag('js', new Date())
-        gtag('config', 'G-FZMR8SL35B')
-      `}
-      </Script>
-
+      <Meta />
       <Component {...pageProps} />
     </>
   )
