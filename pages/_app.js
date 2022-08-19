676a13700a507225d4c699739c126c7112e8855f
Manu Morante <manu@estadologico.com>
2022-08-19 17:50:52 +0200

    Google Analytics script

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,28 @@
+import Script from 'next/script'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      {/* Global site tag (gtag.js) - Google Analytics */}
+      <Script
+        async
+        src='https://www.googletagmanager.com/gtag/js?id=G-FZMR8SL35B'
+      />
+      <Script id='gtag-base' strategy='afterInteractive'>
+        {`
+        window.dataLayer = window.dataLayer || []
+        function gtag() {
+          dataLayer.push(arguments)
+        }
+        gtag('js', new Date())
+        gtag('config', 'G-FZMR8SL35B')
+      `}
+      </Script>
+
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
