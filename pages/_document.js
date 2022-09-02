ddb8c4e5017602b0425f6282700ad10131b77371
Manu Morante <manu@estadologico.com>
2022-09-02 13:17:43 +0200

    bg tests restore

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,8 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import { useRouter } from 'next/router'
 
 export default function Document() {
-  const router = useRouter()
-  const bgcolor = router?.query === 'heart' ? '#f00' : '#000'
-  const style = { backgroundColor: bgcolor }
-  
   return (
-    <Html lang='es' className='transition-colors' style={style}>
+    <Html lang='es' className='transition-colors' style={{ backgroundColor: '#000' }}>
       <Head />
       <body>
         <Main />
