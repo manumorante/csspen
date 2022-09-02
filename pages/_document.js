f0fd2e9aeeac78e1ff69a79fa6ef1dc9898d3ffc
Manu Morante <manu@estadologico.com>
2022-09-02 13:05:56 +0200

    condition red in heart in _document WIP

--- pages/_document.js
+++ pages/_document.js
@@ -1,8 +1,13 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import { useRouter } from 'next/router'
 
 export default function Document() {
+  const router = useRouter()
+  const bgcolor = router.query == 'heart' ? '#f00' : '#000'
+  const style = { backgroundColor: bgcolor }
+  
   return (
-    <Html lang='es' className='transition-colors' style={{ backgroundColor: '#000' }}>
+    <Html lang='es' className='transition-colors' style={style}>
       <Head />
       <body>
         <Main />
