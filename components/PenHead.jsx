7703aac10abc9fcd4cb109d61e27d6cd58b15a6c
Manu Morante <manu@estadologico.com>
2022-09-07 19:35:47 +0200

    OG refactor

--- components/PenHead.jsx
+++ components/PenHead.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import Head from 'next/head'
 
-export default function PenHead({ id, name, bgcolor }) {
-  if (name === '' || bgcolor === '') return null
+export default function PenHead(props) {
+  const title = props.name
+    ? `'${props.name}' y otros logos famosos animados paso a paso con CSS`
+    : `Logos famosos animados paso a paso con CSS`
+  const description = title
+  const url = props.id ? `https://csspen.es/${props.id}` : 'https://csspen.es'
+  const bgcolor = props.bgcolor || '#000000'
 
   if (typeof document !== 'undefined') {
     document.documentElement.style.backgroundColor = bgcolor
@@ -14,13 +19,13 @@ export default function PenHead({ id, name, bgcolor }) {
       <meta name='viewport' content='width=device-width, initial-scale=1.0' />
       <link rel='icon' href='/favicon.ico' />
 
-      <title>{`'${name}' creado y animado paso a paso`}</title>
-      <meta name='description' content={`'${name}' y otros logos famosos animados paso a paso con CSS`} />
+      <title>{title}</title>
+      <meta name='description' content={description} />
       <meta name='theme-color' content={bgcolor} />
 
-      <meta property='og:title' content={`${name} en csspen`} />
-      <meta property='og:url' content={`https://csspen.es/${id}`} />
-      <meta property='og:description' content={`'${name}' y otros logos famosos animados paso a paso con CSS`} />
+      <meta property='og:title' content={title} />
+      <meta property='og:url' content={url} />
+      <meta property='og:description' content={description} />
       <meta property='og:site_name' content='csspen' />
       <meta property='og:type' content='website' />
       <meta property='og:image' content='https://csspen.es/csspen_og.png' />
