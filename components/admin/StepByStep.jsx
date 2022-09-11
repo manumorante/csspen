0de105d78aa82b3567358f78cac6f06d94a8be94
Manu Morante <manu@estadologico.com>
2022-09-11 20:03:06 +0200

    StepByStep

--- components/admin/StepByStep.jsx
+++ components/admin/StepByStep.jsx
@@ -4,12 +4,12 @@ import Cover from '@/Cover'
 export default function StepByStep({ pen }) {
   return (
     <div className='StepByStep'>
-      <div className='Steps flex flex-col gap-6'>
+      <div className='Steps flex flex-wrap gap-6'>
         {pen.steps.map((step, index) => {
           const { info, css } = step
           return (
-            <div className='Step flex gap-4' key={index}>
-              <div className='rounded-lg ' style={{ background: pen.colors.c3 }}>
+            <div className='Step w-44 grow-0 flex flex-col gap-4' key={index}>
+              <div className='rounded-lg' style={{ background: pen.colors.c3 }}>
                 <Cover html={pen.html} css={css} size={176} zoom='0.6' />
               </div>
               <div className='Info text-xl'>{info ? info : <span className='text-red-600'>No info</span>}</div>
