c097cceca2eb958b6e19f9d6c5d633bbae48370c
Manu Morante <manu@estadologico.com>
2022-10-01 19:03:47 +0200

    Add new editor CodeMirror

--- components/admin/StepEditor.jsx
+++ components/admin/StepEditor.jsx
@@ -16,20 +16,14 @@ export default function StepEditor({ i, html, bg, css, onChange }) {
 
   return (
     <div className='StepEditor'>
-      <div className='relative w-80 h-80' style={{ backgroundColor: bg }}>
+      <div className='relative w-full sm:w-80 h-80' style={{ backgroundColor: bg }}>
         <div
           className={`step-${i} absolute inset-0 grid place-items-center w-pen h-pen m-auto`}
           dangerouslySetInnerHTML={{ __html: html }}
         />
       </div>
       <style type='text/css' dangerouslySetInnerHTML={{ __html: scopedCSS }} />
-      <CodeMirror
-        value={css}
-        height='500px'
-        theme={dracula}
-        extensions={[codeCSS()]}
-        onChange={handleOnChange}
-      />
+      <CodeMirror value={css} width='100%' theme={dracula} extensions={[codeCSS()]} onChange={handleOnChange} />
     </div>
   )
 }
